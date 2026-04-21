872b1d145298e699c43298e6fd0ff08c769519a2
opljkl802-bit <opljkl802@gmail.com>
2026-04-20 20:38:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ if __name__ == "__main__" :
     bot = Client(
         "StarkBot",
         bot_token=os.environ.get("BOT_TOKEN"),
-        api_id=(os.environ.get("API_ID")),
+        api_id=os.environ.get("API_ID"),
         api_hash=os.environ.get("API_HASH"),
         sleep_threshold=20,
         plugins=plugins,
