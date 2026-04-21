18b455dfdb76e67e8356a124698f1a35591d49b9
opljkl802-bit <opljkl802@gmail.com>
2026-04-20 20:04:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ logging.basicConfig(
     ],
 )
 
-# Auth Users
-AUTH_USERS = [ int(chat) for chat in Config.AUTH_USERS.split(",") if chat != '']
 
 # Prefixes 
 prefixes = ["/", "~", "?", "!"]
@@ -53,7 +51,7 @@ if __name__ == "__main__" :
     bot = Client(
         "StarkBot",
         bot_token=os.environ.get("BOT_TOKEN"),
-        api_id=int(os.environ.get("API_ID")),
+        api_id=(os.environ.get("API_ID")),
         api_hash=os.environ.get("API_HASH"),
         sleep_threshold=20,
         plugins=plugins,
