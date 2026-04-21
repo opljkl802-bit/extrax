17ea25a19d1aa2aba926de95594a4239763fc742
opljkl802-bit <opljkl802@gmail.com>
2026-04-20 21:36:11 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID    = os.environ.get("30296254", "")
+API_ID    = int(os.environ.get("30296254", ""))
 API_HASH  = os.environ.get("c2b5306f4ccd2d795405a026c10b4c62", "")
 BOT_TOKEN = os.environ.get("8697727988:AAHFPX-fMxt61jqOyFlJEbLJO7EwR1TEP4M", "")
 AUTH_USERS = "7660916897"
