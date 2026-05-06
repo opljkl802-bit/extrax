b1d14e304b73c54bde6ba811a7a932f2d2c00f73
opljkl802-bit <opljkl802@gmail.com>
2026-05-05 23:58:12 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
-
-API_ID    = int(os.environ.get("30296254", ""))
-API_HASH  = os.environ.get("c2b5306f4ccd2d795405a026c10b4c62", "")
-BOT_TOKEN = os.environ.get("8697727988:AAHFPX-fMxt61jqOyFlJEbLJO7EwR1TEP4M", "")
-AUTH_USERS = "7660916897"
+from os import getenv
+# ---------------R---------------------------------
+API_ID = int(os.environ.get("API_ID", "30296254"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
+# ----------------D--------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8614201321:AAFYogtipBDjlWBhTQGXJTcYuX3co4S0t4c")
+# ------------------X------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "7660916897"))
