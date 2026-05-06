2c6f5ce56bd07b0888258156ffdc7717f4d9ff48
opljkl802-bit <opljkl802@gmail.com>
2026-05-06 00:15:57 -0700

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 
-@stark.on_message(filters.command(["start"]) & ~filters.edited)
+@stark.on_message(filters.command(["start"]))
 async def Start_msg(bot: stark , m: Message):
     await bot.send_photo(
     m.chat.id,
@@ -32,7 +32,7 @@ async def Start_msg(bot: stark , m: Message):
            
 
 
-@stark.on_message(filters.command(["restart"]) & ~filters.edited)
+@stark.on_message(filters.command(["restart"]))
 async def restart_handler(_, m):
     await m.reply_text("Restarted!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
