6c5630a8e0baac008d0eae9f8ab940477370e60a
opljkl802-bit <opljkl802@gmail.com>
2026-05-06 00:04:28 -0700

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ API_HASH = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8614201321:AAFYogtipBDjlWBhTQGXJTcYuX3co4S0t4c")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "7660916897"))
+AUTH_USERS = [OWNER_ID]
